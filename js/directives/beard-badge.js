125f975a6a4557b0219a472c6cf3a40c5f2ff952
Nathanael Farley <nasfarley88@gmail.com>
2017-03-11 09:37:49 +0000

    HTML formatting now supported.

--- js/directives/beard-badge.js
+++ js/directives/beard-badge.js
@@ -2,6 +2,16 @@
   // Grab the app
   var app = angular.module( "APIBeardDemo");
 
+  app.filter('trust', [
+    '$sce',
+    function($sce) {
+      return function(value, type) {
+        // Defaults to treating trusted text as `html`
+        return $sce.trustAs(type || 'html', text);
+      };
+    }
+  ]) ;
+
   app.directive('beardBadge', function(){
     return {
       restrict: "E",
