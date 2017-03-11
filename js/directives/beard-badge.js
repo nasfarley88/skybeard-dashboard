83850c1d2d88f1756789c358ae6aecce85f059a0
Nathanael Farley <nasfarley88@gmail.com>
2017-03-11 16:14:45 +0000

    Finalised beard badges.

--- js/directives/beard-badge.js
+++ js/directives/beard-badge.js
@@ -2,16 +2,6 @@
   // Grab the app
   var app = angular.module( "APIBeardDemo");
 
-  app.filter('trust', [
-    '$sce',
-    function($sce) {
-      return function(value, type) {
-        // Defaults to treating trusted text as `html`
-        return $sce.trustAs(type || 'html', text);
-      };
-    }
-  ]) ;
-
   app.directive('beardBadge', function(){
     return {
       restrict: "E",
